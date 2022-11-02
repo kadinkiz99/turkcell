3f7bbe995668d9335a10602c885d62798597695a
Kadınkız Önal <gazze4488@gmail.com>
2022-11-02 12:42:43 +0300

    Database firs yaklaşımı ile veri tabanında değişiklik yapıldı

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,6 +6,9 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public string? color  { get; set; }
+		public int? Width { get; set; }// ? nullable olduğu için eklendş 
+		public int? Height { get; set; }
 
-    }
+	}
 }
